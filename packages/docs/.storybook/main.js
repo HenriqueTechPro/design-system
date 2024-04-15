172e1b9eb82b5dfc99b619d4334237e47ea24c7e
HenriqueTechPro <devopsninjapro@gmail.com>
2024-04-15 11:13:13 -0300

    update

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,17 +1,19 @@
-import { dirname, join } from "path";
 module.exports = {
-  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
 
   "addons": [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    getAbsolutePath("@chromatic-com/storybook"),
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@chromatic-dom/storybook"
   ],
+
   "framework": {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {}
   },
 
@@ -19,8 +21,8 @@ module.exports = {
     "storyStoreV7": true
   },
 
-  viteFinal: (config, {configType}) => {
-    if(configType === 'PRODUCTION') {
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
 
@@ -29,9 +31,9 @@ module.exports = {
 
   docs: {
     autodocs: true
-  }
-}
+  },
 
-function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 }
